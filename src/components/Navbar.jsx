8c8efe1d42117272f060bad0b22cbad1262ac629
Kenneth <quibelkenneth@gmail.com>
2024-03-01 20:17:47 +0800

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,35 @@
+import { useState } from 'react';
 import {Link, NavLink} from 'react-router-dom'
-import Logo from '../images/logo.jpg'
+import Logo from '../images/logobtc.png'
 import {links} from '../data'
 import { FaBars } from "react-icons/fa6";
+import { MdOutlineClose } from "react-icons/md";
 import './navbar.css'
 
 const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
   return (
     <nav>
         <div className="container nav_container">
             <Link to="/" className='logo'>
-                <img src={Logo} alt="BitcoinForecast Logo" width="100px" />
+                <img src={Logo} alt="BitcoinForecast Logo" className="logo-img" />
             </Link>
-            <ul className='nav_links'>
+            <ul className={`nav_links ${isNavShowing ? 'show_nav' : 'hide_nav'}`}>
                 {
                     links.map(({name,path}, index) => {
                         return (
                             <li>
-                                <NavLink to={path}>{name}</NavLink>
+                                <NavLink to={path} className={({isActive}) => isActive  ? 'active-nav' : ''}>{name}</NavLink>
                             </li>
                         )
                     })
                 }
             </ul>
-            <button className="nav_toggle-btn">
-                <FaBars />
+            <button className="nav_toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+                {
+                    isNavShowing ? <MdOutlineClose/>  : <FaBars />
+                }
             </button>
         </div>
     </nav>
