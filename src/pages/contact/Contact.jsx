fccff2d4e27761c561f222323ba1ade67661034b
Kenneth <quibelkenneth@gmail.com>
2024-03-20 18:30:33 +0800

    Contact us page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,26 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/bg1.png'
+import { FaFacebookF } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
 import './contact.css'
 
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Header title="Get In Touch" image={HeaderImage}>
+     Have a question, feedback, or just want to say hello? We'd love to hear from you!
+     Feel free to reach out to us via the following channels:
+    </Header>
+    <section className='contact'>
+      <div className="container contact_container">
+        <div className="contact_wrapper">
+          <a href="https://t.me/AstraderSnipe" target='_blank' rel='noreferrer noopen'><FaTelegram/></a>
+          <a href="https://www.facebook.com/BitFor" target='_blank' rel='noreferrer noopen'><FaFacebookF/></a>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
