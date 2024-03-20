00ca62de86432f81bb97ee73a740a018ce4ddeed
Kenneth <quibelkenneth@gmail.com>
2024-03-20 17:16:15 +0800

    about us

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,9 @@
 import Header from '../../components/Header'
 import './about.css'
 import HeaderImage from '../../images/chart.jpg'
-import ApproachImage from '../../images/astro.jpg'
+import ApproachImage from '../../images/astro2.png'
+import MentorImage from '../../images/mentor1.png'
+import PrivateImage from '../../images/private.jpg'
 
 
 const About = () => {
@@ -19,6 +21,33 @@ const About = () => {
           <h1>Our Approach</h1>
           <p>
           At BitcoinForecast, we believe in harnessing diverse methodologies to provide insightful predictions in the volatile world of cryptocurrency. Our unique blend of astrology, technical analysis, and strategic insights sets us apart in the field of forecasting. By combining these unconventional approaches with traditional methods, we aim to offer our members a comprehensive understanding of Bitcoin market trends.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section className="about_engagement">
+      <div className="about_engagement-container">
+        
+          <div className="about_section-content">
+            <h1>Community Engagement</h1>
+            <p>
+            Our commitment to fostering a community of knowledgeable cryptocurrency enthusiasts is at the heart of BitcoinForecast. Through our private Facebook group, members gain exclusive access to our predictions, discussions, and insights. This platform serves as a hub for sharing ideas, learning from each other, and staying ahead in the ever-evolving landscape of digital assets.
+            </p>
+        </div>
+        <div className="about_section-img">
+          <img src="https://cdn.pixabay.com/photo/2022/10/04/22/43/crowd-7499209_1280.png" alt="Community Engagement Image" />
+        </div>
+      </div>
+    </section>
+    <section className="about_education">
+      <div className="about_education-container">
+        <div className="about_section-img">
+          <img src={MentorImage} alt="Empowering Education Image" />
+        </div>
+        <div className="about_section-content">
+          <h1>Empowering Education</h1>
+          <p>
+          Beyond predictions, BitcoinForecast is dedicated to empowering individuals with the knowledge and skills to navigate the cryptocurrency market confidently. Through our free mentorship sessions on YouTube, we provide accessible guidance and educational resources. Whether you're a seasoned trader or just starting your journey in crypto, our mentorship program offers valuable insights to help you make informed decisions and achieve your financial goals.
           </p>
         </div>
       </div>
